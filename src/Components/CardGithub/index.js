5b389d3a0846a1d2e874a41280fb4d63b62e8f84
Munkh-Erdene <munkh-erdene@education.icodice.com>
2022-11-30 23:46:24 -0600

    style change

--- src/Components/CardGithub/index.js
+++ src/Components/CardGithub/index.js
@@ -1,17 +1,33 @@
 import React from 'react'
 import './style.css'
+import ghlogo from './images/ghlogo.png'
 
 const AccountCard = ({person}) => {
 
     return (
-        <>
-            <div>{person.login}</div>
-            <img src={person.avatar_url} height={100} alt=''/>
-            <div>{person.bio}</div>
-            <div>{person.followers}</div>
-            <div>{person.following}</div>
-            <div>{person.public_repos}</div>
-        </>
+        <div className='github-container'>
+            <div className='github-card'>
+                <div className='github-logo-div'><img className='github-logo' src={ghlogo} height={25} width={25} alt='ghlogo'/></div>
+                <img className='profile-image' src={person.avatar_url} height={150} width={150} alt='profile'/>
+                <div className='github-name'>{person.name}</div>
+                <div className='github-login'>@{person.login}</div>
+                <div className='github-bio'>{person.bio}</div>
+                <div className='github-bottom-card'>
+                    <div className='github-followers'>
+                        <div className='github-bottom-number'>{person.followers}</div>
+                        <div className='github-bottom-text'>Followers</div> 
+                    </div>
+                    <div className='github-followers'>
+                        <div className='github-bottom-number'>{person.following}</div>
+                        <div className='github-bottom-text'>Following</div>
+                    </div>
+                    <div className='github-followers'>
+                       <div className='github-bottom-number'>{person.public_repos}</div>
+                        <div className='github-bottom-text'>Repositories</div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 export default AccountCard 
--- src/Components/navBar/Navbar.js
+++ src/Components/navBar/Navbar.js
@@ -6,15 +6,15 @@ const Navbar = () => {
 
   return (
     <div className="navbar-layout">
-      <button onClick={()=>navigate("/memorygame")}>Memory Game</button>
-      <button onClick={()=>navigate("/tictactoe")}>Tic Tac Toe</button>
-      <button onClick={()=>navigate("/colorpicker")}>Color Picker</button>
-      <button onClick={()=>navigate("/liveclock")}>Live Clock</button>
-      <button onClick={()=>navigate("/imageslider")}>Image Slider</button>
-      <button onClick={()=>navigate("/todo")}>To Do</button>
+      <button onClick={() => navigate("/memorygame")}>Memory Game</button>
+      <button onClick={() => navigate("/tictactoe")}>Tic Tac Toe</button>
+      <button onClick={() => navigate("/colorpicker")}>Color Picker</button>
+      <button onClick={() => navigate("/liveclock")}>Live Clock</button>
+      <button onClick={() => navigate("/imageslider")}>Image Slider</button>
+      <button onClick={() => navigate("/todo")}>To Do</button>
+      <button onClick={() => navigate('/githubaccount')}>Git Hub Account</button>
       <Outlet />
     </div>
   )
-};
-
+}
 export default Navbar;
